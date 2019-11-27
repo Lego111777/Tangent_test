67e01a90eee64fe711a841e8d80369fde3d50173
Gregory Reed <grgregoryreed@gmail.com>
2019-11-27 20:42:43 +0200

    added models for Employee and Leave Tables

--- LeaveLogger/models.py
+++ LeaveLogger/models.py
@@ -1,3 +1,24 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+STATUS_CHOICES = (
+    ('new', 'New'),
+    ('app', 'Approved'),
+    ('decl', 'Declined'),
+)
+
+
+class Employee(models.Model):
+    emp_number = models.CharField(max_length=10)
+    phone_number = models.IntegerField()
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+
+class Leave(models.Model):
+    employee_pk = models.ForeignKey(emp_number, on_delete=models.CASCADE)
+    start_date = models.DateFimeField()
+    end_date = models.DateFimeField()
+    days_of_leave = models.IntegerField()
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='new')
--- Tangent/settings.py
+++ Tangent/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'LeaveLogger.apps.LeaveloggerConfig',
 ]
 
 MIDDLEWARE = [
